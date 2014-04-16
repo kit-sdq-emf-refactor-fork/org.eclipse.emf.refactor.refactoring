5dcebee0ca82c572b75b152ec2ff6cbcf19e0804
tarendt <tarendt>
2014-04-16 13:02:18 +0200

    bug fixed

--- org.eclipse.emf.refactor.comrel/src/comrel/util/ExtensionPointInfo.java
+++ org.eclipse.emf.refactor.comrel/src/comrel/util/ExtensionPointInfo.java
@@ -7,10 +7,10 @@ public class ExtensionPointInfo {
 //	public static final String SINGLEFEATURE_EXTENSION_POINT = "org.eclipse.emf.refactor.singlefeature";
 //	public static final String MULTIFEATURE_EXTENSION_POINT = "org.eclipse.emf.refactor.multifeature";
 	
-	public static final String SINGLEFILTER_EXTENSION_POINT = "comrel.singlefilter";
-	public static final String MULTIFILTER_EXTENSION_POINT = "comrel.multifilter";
-	public static final String SINGLEFEATURE_EXTENSION_POINT = "comrel.singlefeature";
-	public static final String MULTIFEATURE_EXTENSION_POINT = "comrel.multifeature";
+	public static final String SINGLEFILTER_EXTENSION_POINT = "org.eclipse.emf.refactor.comrel.singlefilter";
+	public static final String MULTIFILTER_EXTENSION_POINT = "org.eclipse.emf.refactor.comrel.multifilter";
+	public static final String SINGLEFEATURE_EXTENSION_POINT = "org.eclipse.emf.refactor.comrel.singlefeature";
+	public static final String MULTIFEATURE_EXTENSION_POINT = "org.eclipse.emf.refactor.comrel.multifeature";
 
 	public static final String SINGLEFEATURE = "singlefeature";
 	public static final String MULTIFEATURE = "multifeature";
